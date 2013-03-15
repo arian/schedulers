d97261b7cca57e0ea23dfc6dbe56f2b4dd106d43
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-03-15 01:42:56 +0100

    Round robin is now using tweets as input

--- rr.rb
+++ rr.rb
@@ -1,14 +1,46 @@
 
+require 'tweetstream'
 require "./tasks"
+require './configure'
 
-# main loop
+# tweet is basically some peripheral register here
+$tweet = ""
 
-while true
+$word2 = "one direction" # too much tweets
+$word1 = "bieber" # way way too much
+$word3 = "muse" # not enough
 
-	char = STDIN.getc
-
-	Tasks.A if char == "a"
-	Tasks.B if char == "b"
-	Tasks.C if char == "c"
-
+# this fetches tweets through some stream and calls the isr
+p1 = Thread.new do
+	TweetStream::Client.new.track($word1, $word2, $word3) do |status|
+		$tweet = status.text.downcase
+	end
 end
+
+p2 = Thread.new do
+	loop do
+
+		# save in local variable to prevent shared data bug
+		tweet = $tweet
+		if not tweet.index($word1).nil?
+			puts tweet.cyan
+			Tasks.A
+		end
+
+		tweet = $tweet
+		if not tweet.index($word2).nil?
+			puts tweet.cyan
+			Tasks.B
+		end
+
+		tweet = $tweet
+		if not tweet.index($word3).nil?
+			puts tweet.cyan
+			Tasks.C
+		end
+
+	end
+end
+
+p2.join
+p1.join
