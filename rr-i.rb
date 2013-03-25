8319794a87516c1608a0343142f585473edd32f2
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-03-25 13:58:53 +0100

    Add some comments to rr-i

--- rr-i.rb
+++ rr-i.rb
@@ -3,19 +3,14 @@ require 'tweetstream'
 require "./tasks"
 require './configure'
 
-# tweet is basically some peripheral register here
+# tweets is basically some peripheral register here
 $tweets = Array.new(3)
+# flag is set in the isr for this kind of tweet
 $flags = Array.new(3).map {|f| false }
+# search words
 $words = ["one direction", "bieber", "muse"]
+# tasks that can be executed in the main loop if a flag is set to true
 $tasks = [-> { Tasks.A }, -> { Tasks.B }, -> { Tasks.C }]
-
-# interrupt service routine that sets the flags
-$isrs = $flags.each_with_index.map do |f, i|
-	-> {
-		puts $tweets[i].cyan
-		$flags[i] = true
-	}
-end
 
 # this fetches tweets through some stream and puts them in the $tweets "register"
 
@@ -26,6 +21,14 @@ p = $words.each_with_index.map do |word, i|
 			$isrs[i].call
 		end
 	end
+end
+
+# interrupt service routine that sets the flags
+$isrs = $flags.each_with_index.map do |f, i|
+	-> {
+		puts $tweets[i].cyan
+		$flags[i] = true
+	}
 end
 
 # main loop
