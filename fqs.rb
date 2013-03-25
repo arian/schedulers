799dc591b50894919247e73453651d4d7a11d983
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-03-25 13:58:28 +0100

    Implement tweetstream for fqs

--- fqs.rb
+++ fqs.rb
@@ -1,29 +1,45 @@
 
+require 'tweetstream'
+require 'thread'
 require "./tasks"
-require "thread"
+require './configure'
 
-# new thread that reads stdin (the while blocks other stuff)
-p1 = Thread.new do
-	isr_char while $char = STDIN.getc
+# tweets is basically some peripheral register here
+$tweets = Array.new(3)
+# words to search for on twitter
+$words = ["one direction", "bieber", "muse"]
+# task that can be called
+$tasks = [-> { Tasks.A }, -> { Tasks.B }, -> { Tasks.C }]
+
+# this fetches tweets through some stream and puts them in the $tweets "register"
+
+p = $words.each_with_index.map do |word, i|
+	Thread.new do
+		TweetStream::Client.new.track(word) do |status|
+			$tweets[i] = status.text
+			$isrs[i].call
+		end
+	end
 end
 
 # function queue
 $queue = Queue.new
 
-# interrupt service routine
-def isr_char
-	$queue.push(Tasks.A) if $char == 'a'
-	$queue.push(Tasks.B) if $char == 'b'
-	$queue.push(Tasks.C) if $char == 'c'
+# interrupt service routine that puts the task in the queue
+$isrs = $flags.each_with_index.map do |f, i|
+	-> {
+		puts $tweets[i].cyan
+		$queue.push($tasks[i])
+	}
 end
 
 main = Thread.new do
 	# main loop
 	loop do
 		task = $queue.pop
-		task
+		task.call
 	end
 end
 
-p1.join
+p.join
 main.join
